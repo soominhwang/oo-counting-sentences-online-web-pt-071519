df3607e0bbe093c2b796ce7b8d1bd3f19de6edbd
Soomin Hwang <soomin.h.hwang@gmail.com>
2019-08-19 01:49:58 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,7 +16,7 @@ class String
 
   def count_sentences
     sentences = []
-      sentences << self.replace('!',' ').replace('?',' ').replace('.',' ').split
+    self.replace('!',' ').replace('?',' ').replace('.',' ').split
     sentences.count
   end
 end
