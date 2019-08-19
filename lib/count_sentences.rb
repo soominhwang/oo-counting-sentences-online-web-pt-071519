d129ce65058b1bd97c2098bcd05adb52764a7734
Soomin Hwang <soomin.h.hwang@gmail.com>
2019-08-19 01:27:57 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.include?("?")
   end
 
   def exclamation?
-
+    self.include?("!")
   end
 
   def count_sentences
-
+    count_sentences = []
+    delimiters = ["!",".","?"]
+    count_sentences << self.split(Regexp.union(delimiters))
+    count_sentences.count
   end
 end
