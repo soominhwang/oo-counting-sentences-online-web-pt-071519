6fbc3400f1c2c511eb136ba291df0c720bcb54f0
Soomin Hwang <soomin.h.hwang@gmail.com>
2019-08-19 02:21:23 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,29 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.include?("?")
   end
 
   def exclamation?
-
+    self.include?("!")
   end
 
   def count_sentences
-
+    # sentences = []
+    # sentences << self.replace('!',' ').replace('?',' ').replace('.',' ').split
+    # sentences.count
+    sentences = []
+    self.split('!').each do |sentence_excl|
+      sentence_excl.split('.').each do |sentence_excl_per|
+        sentence_excl_per.split('?').each do |final_sentence|
+          sentences << final_sentence
+        end
+      end
+    end
+    sentences.count
   end
 end
