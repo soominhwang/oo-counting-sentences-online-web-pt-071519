167c520976a16b899e5e6835132866f210326da1
Soomin Hwang <soomin.h.hwang@gmail.com>
2019-08-19 02:04:54 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -19,7 +19,7 @@ class String
     # sentences << self.replace('!',' ').replace('?',' ').replace('.',' ').split
     # sentences.count
     sentences = []
-    sentences = re.split('[?!.]', self)
+    sentences = self.split('[?!.]')
     sentences.count
   end
 end
