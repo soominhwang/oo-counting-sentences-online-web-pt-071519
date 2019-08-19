23c8ab9016e510981715c9e1c2b6edfe2345d17a
Soomin Hwang <soomin.h.hwang@gmail.com>
2019-08-19 02:11:38 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -24,8 +24,13 @@ class String
 
     sentences = []
 
-    sentences = self.split('?')
-
+    self.split('!').each do |sentence_excl|
+      sentence_excl.split('.').each do |sentence_excl_per|
+        sentence_excl_per.split('?').each do |final_sentence|
+          sentences << final_sentence
+        end
+      end
+    end
 
     sentences.count
   end
